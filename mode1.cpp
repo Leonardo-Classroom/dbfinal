d8e785b2364bceaf94a867b71914b7969c90c1e0
williamimmanuelabadi <williamimmanuelabadi1@gmail.com>
2023-06-12 19:12:41 +0700

    Update mode1.cpp

--- mode1.cpp
+++ mode1.cpp
@@ -9,23 +9,18 @@ using namespace std;
 
 void mode1(vector<string> result){
 
-    cout<<"mode 1"<<endl;
+    cout<<"mode 1: "<<endl;
 
     if(result.size()){
         cout<<"result:"<<endl;
-        cout<<result[0]<<"-->";
+        cout<<result[0]<<"> ";
         for(int i=1;i<result.size();i++){
-            if(i==1)
-            	cout<<"\n";
-            cout<<i<<". ";
-			if(i<10)
-            	cout<<" ";
-			cout<<result[i]<<" \n";
+			cout<<result[i]<<" ";
         }
         cout<<endl;
         cout<<"==============="<<endl;
 
-        //寫這邊，result[0]是學號，result[1]...是block，代表在result[0]哪些block中有資料
-        //去這些block中找資料，把該學號修的課號跟課名都印出來
+        //Result[0] is StudentId, result[1]... are blocks, represent there is data in which block in result[0]
+        //Go to these block to find information, and print out the classId
     }
 }

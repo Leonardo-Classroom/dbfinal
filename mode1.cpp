68b19069d8481ae620e6305626b991d1e2076409
williamimmanuelabadi <williamimmanuelabadi1@gmail.com>
2023-06-13 09:43:39 +0700

    Update mode1.cpp

--- mode1.cpp
+++ mode1.cpp
@@ -38,41 +38,22 @@ void mode1(vector<string> result){
 
         for(int i=1; i<result.size(); i++)
         {
-            // if(courseId==result[0] && !isDataExist)
-                // cout<<
-            // cout<<"block"+result[i]+":-------"<<endl;
-
             string block_str="block"+result[i]+".csv";
             ifstream file("blocks/"+block_str);
             string line;
             getline(file, line);
             while(getline(file, line)){
                 vector<string> tokens=sssplit(line, ',');
-                // if don't use sssplit
-                // string token;
-                // istringstream tokenstream(line);
-                // while(getline(tokenstream, token, ',')){
-                //     tokens.push_back(token);
-                //     // cout<<token<<", ";
-                //     // system("pause");
-                // }
-
                 if(tokens[0] == result[0]){
-
                     if(!isStart){
                         cout<<"student_ID\n ["<<tokens[0]<<"]"<<endl;
                         isStart=true;
                         cout<<"\ncourse_ID\tcourse_Name\t\tblock_Num\n"<<endl;
                     }
-
-                    // isDataExist = 1;
                     cout<<tokens[1]<<"\t\t"<< tokens[2]<<"\t\t[block "<< result[i]<<"]"<< endl;
-
-                    // courseId = token[1];
                 }
-                // system("pause");
             }
-            // system("pause");
         }
+        cout<<"==============="<<endl;
     }
 }
